bd09e1652b55000fe02d9b2c6b777fcae1d448e4
moria0525 <moria0525@gmail.com>
2024-02-27 19:49:59 +0200

    Add existing project files to Git

--- server.js
+++ server.js
@@ -24,12 +24,11 @@ import { userRoutes } from './api/user/user.routes.js'
 app.use('/api/bug', bugRoutes)
 app.use('/api/user', userRoutes)
 
-const port = 3000
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
+console.log(`App listening on port ${port}!`)
+});
 
-    loggerService.info('Up and running on port', port)
-})
 
 
 
